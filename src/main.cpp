3cd087a828a6d46a8efcb25c1479154c5f12437f
arne <arne@rnae.de>
2024-12-22 13:53:39 +0100

    feat: add hotspot mode for configuration

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,17 @@
 #include "IOHandler.hpp"
 #include "Ui.hpp"
 #include <ESPmDNS.h>
+#include <DNSServer.h>
 
 #define SSID "Unten"
 #define PASS "XBueLkpWnLUdeffx"
+#define AP_SSID "ArtNet-GPIO"
+
+#define HOTSPOT_MODE_PIN 25
 
 // put function declarations here:
+void setupNormalMode(Config *cfg);
+void setupHotspotMode();
 
 Preferences preferences;
 Config *config;
@@ -17,7 +23,9 @@ Network *network;
 IOHandler *ioHandler;
 Ui *ui;
 
-void setupNetwork(Config *cfg)
+DNSServer *dnsServer; // this is only used in hotspot mode
+
+void setupNormalMode(Config *cfg)
 {
   if (!cfg->getUseDHCP())
   {
@@ -58,17 +66,47 @@ void setupNetwork(Config *cfg)
   Serial.println(hostname + ".local");
 }
 
+// hotspot mode is used as fallback for configuration.
+// It is not intended to be used as a permanent mode.
+void setupHotspotMode()
+{
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(AP_SSID); // Remove password parameter to create an open network
+
+  Serial.println("Access Point Mode");
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  dnsServer = new DNSServer();
+  dnsServer->setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer->setTTL(300);
+  // DNSServer is started serving any "*" domain name. It will reply
+  // AccessPoint's IP to all DNS request and the ESPUI will catch all requests and redirect them to the root page
+  dnsServer->start(53, "*", WiFi.softAPIP());
+}
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Starting gpio artnet sender");
 
+  pinMode(HOTSPOT_MODE_PIN, INPUT_PULLUP);
+
   preferences.begin("cfg", false);
   config = new Config(preferences);
   config->print();
 
-  setupNetwork(config);
-
+  const bool enableHotspot = digitalRead(HOTSPOT_MODE_PIN) == LOW;
+  if (enableHotspot)
+  {
+    setupHotspotMode();
+  }
+  else
+  {
+    // Normal mode
+    setupNormalMode(config);
+  }
   const auto targetIp = WiFi.broadcastIP();
   network = new Network(config->getUniverse(), targetIp.toString());
 
@@ -81,6 +119,12 @@ void setup()
 
 void loop()
 {
+  // dnsServer only exists when in hotspot config mode
+  if (dnsServer)
+  {
+    dnsServer->processNextRequest();
+  }
+
   ioHandler->update();
   network->update();
 }
